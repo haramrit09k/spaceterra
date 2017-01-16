312a178425f4f9316adba3399439ae3be289e3ff
haramrit09k <haramrit09k@gmail.com>
2017-01-17 00:20:11 +0530

    Oscillation achieved
    Oscillates even without giving input, Yay!

--- game.js
+++ game.js
@@ -1,108 +1,120 @@
-var game = new Phaser.Game(1366, 768, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+/* global console */
+/* global Phaser */
 
-function preload() {
+(function() {
+    'use strict';
 
-    game.load.image('rocket', 'rocket.png');
-    game.load.image('obstacle', 'obstacle.png')
-    game.load.image('sky', 'sky.png');
+    var game, rocket, obstacle, cursors, i, y, sky;
+    var oscIndex = 1;
 
-}
+    var preload = function preload() {
+         game.load.image('rocket', 'rocket.png');
+         game.load.image('obstacle', 'obstacle.png');
+         game.load.image('sky', 'sky.png');
+    };
 
-var tweenA, rocket, obstacle, cursors, i, y;
+    var create = function create() {
+        // game.physics.startSystem(Phaser.Physics.ARCADE);
+        //Started P2 Physics System
+        game.physics.startSystem(Phaser.Physics.P2JS);
 
-function create() {
+        this.game.scale.pageAlignHorizontally = true;this.game.scale.pageAlignVertically = true;this.game.scale.refresh();
 
-    // game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.startSystem(Phaser.Physics.P2JS);	
-	game.world.resize(6000,6000);
-	var sky = game.add.tileSprite(0,0,1920,1920,'sky');
-    rocket = game.add.sprite(300,5500,'rocket');
-    rocket.scale.setTo(0.3,0.3);
-    sky.scale.setTo(7,7);
-   	// rocket.fixedToCamera = true;
-   //tweenA = game.add.tween(rocket).to( { angle: 45 }, 2, "Quart.easeOut").to( { x: 550, y:700 }, 1200, "Quart.easeInOut").to( { angle: 0 }, 20, "Quart.easeOut").to( { angle: -45 }, 20, "Quart.easeOut").to( { x: 30 }, 1200, "Quart.easeInOut").to( { angle: 0 }, 20, "Quart.easeOut").loop(true);
+        //Resized game world
+        // game.world.resize(6000, 6000);
+        //Add sprites
+        sky = game.add.tileSprite(0, 0, 800, 600, 'sky');
+        sky.tileScale.set(2, 2);
+        rocket = game.add.sprite(400, 500, 'rocket');
+        //Scale sprites
+        rocket.scale.setTo(0.3, 0.3);
 
- 	
- 	//game.input.onDown.addOnce(start, this);
+        // rocket.x.fixedToCamera = true;
+        //tweenA = game.add.tween(rocket).to( { angle: 45 }, 2, "Quart.easeOut").to( { x: 550, y:700 }, 1200, "Quart.easeInOut").to( { angle: 0 }, 20, "Quart.easeOut").to( { angle: -45 }, 20, "Quart.easeOut").to( { x: 30 }, 1200, "Quart.easeInOut").to( { angle: 0 }, 20, "Quart.easeOut").loop(true);
+        //game.input.onDown.addOnce(start, this);
+        //obstacles = game.add.group();
+        //    obstacles.enableBody = true;
+        //      x = 40;
+        //    for(var i=0; i<5; i++){
+        //    var obstacle = obstacles.create(x, game.world.randomY, 'obstacle');
+        //    x += game.rnd.integerInRange(100,250);
+        // }
+        // obstacles.outOfBoundsKill = true;
 
- 	//obstacles = game.add.group();
+        rocket.enableBody = true;
 
- //    obstacles.enableBody = true;
+        //Camera follows rocket
+        // game.camera.follow(rocket);
 
- //   	x = 40;
+        cursors = game.input.keyboard.createCursorKeys();
+        // console.log(rocket.y);
+    };
 
- //    for(var i=0; i<5; i++){
-    	
- //    var obstacle = obstacles.create(x, game.world.randomY, 'obstacle');    
-	
-	// //obstacle.body.velocity.y = 0;
-    
- //    x += game.rnd.integerInRange(100,250);
-    
-	// }
-	// obstacles.outOfBoundsKill = true;
+    var update = function update() {
+        //obstacles.forEach(checkPos, this);
 
-	// game.physics.p2.enable(rocket);
+          //  Scroll the background
+            
+        
+        // game.physics.arcade.overlap(rocket, obstacles, collisionHandler, null, this);
 
-	game.camera.follow(rocket);
+        //On pressing the up arrow key, the rocket moves forward in a sine wave path
+        if (cursors.up.isDown) {
+            //game.camera.y -= 5;
+            rocket.y -= 5;
+            sky.tilePosition.y += 2;
+            // rocket.body.moveUp(200);
+        } else if (cursors.down.isDown) {
+            //game.camera.y -= 5;
+            rocket.y += 5;
+            // rocket.body.moveUp(200);
+        }
+        oscillation(); //Not working :(
+    };
 
-    cursors = game.input.keyboard.createCursorKeys();
-    // console.log(rocket.y);
-}
+    // var checkPos = function checkPos(obstacle) {
+    //     if (obstacle.y > 800) {
+    //         obstacle.y = -100;
+    //     }
+    // };
 
-// function start() {
+    // var collisionHandler = function collisionHandler(rocket, obstacle) {
+    //     rocket.x = 300;
+    //     rocket.y = 700;
+    // };
 
-//     tweenA.start();
+    var calcSin = function calcSin (amp, osc, sinD) {
+        return amp * Math.sin(osc / sinD);
+    };
 
-// }
-
-function update() {
-    //obstacles.forEach(checkPos, this);
-
-   // game.physics.arcade.overlap(rocket, obstacles, collisionHandler, null, this);
-
-        if (cursors.up.isDown)	{
-    	//game.camera.y -= 5;
-		rocket.x = 600 + (200*Math.sin(rocket.y/50));
-    	rocket.y -= 5;
-  		// rocket.body.moveUp(200);
-    							}
-    else{
-			oscillation();	
-		}	
-		console.log(y);
-    }
-
-    
-
-
-
-// function checkPos (obstacle) {
-
-//     if (obstacle.y > 800)
-//     {
-//         obstacle.y = -100;
-//     }
-
-// }
+    //Oscillation function defined separately to oscillate the particle
+    var oscillation = function oscillation() {
+        var y = rocket.y;
+        if (oscIndex === 10000000) oscIndex = 1;
+        var sin = calcSin(300, oscIndex, 40);
+        rocket.x = 400 + parseInt(sin);
+        y = y - 2;
+        oscIndex++;
+        console.log(oscIndex);
+        // console.log(rocket.x);
+    };
 
 
-// function collisionHandler (rocket, obstacle) {
 
-//     rocket.x = 300;
-//     rocket.y = 700;
+    window.onload = function() {
+      var cp = Phaser.Tile.prototype.containsPoint;
+      game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', {
+        preload: preload,
+        create: create,
+        update: update,
+      });
+      // game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+      //   preload: preload,
+      //   create: create,
+      //   update: update,
+      // });
+    };
 
-// }
-
-function oscillation(){
-
-    y = rocket.y;
-	while(i<300)
-	{
-    rocket.x = 600 + (400*Math.sin(y/50));
-    i = i + 1;
-    y = y - 3; 
-
-}
-
-}
+    window.onload();
+})();
+ 
